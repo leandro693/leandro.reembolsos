db6c24e813af77bfa0fa09e06d0849e85312a9c6
Leandro Freitas <leandro@maradelcontabil.com>
2026-08-11 18:40:22 -0300

    chore: remove SQL transitorio

--- supabase/auditoria/confirma-orfa.sql
+++
@@ -1,5 +0,0 @@
-select json_build_object(
-  'auth', (select count(*) from auth.users where email='orfa.teste@maradel.com.br'),
-  'usuarios', (select count(*) from public.usuarios where email='orfa.teste@maradel.com.br'),
-  'empresa_usuarios', (select count(*) from public.empresa_usuarios eu join public.usuarios u on u.id=eu.usuario_id where u.email='orfa.teste@maradel.com.br')
-) as res;
