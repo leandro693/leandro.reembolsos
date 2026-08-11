887b97456ad6caacd788e5eee13d88ac30c2e5b8
Leandro Freitas <leandro@maradelcontabil.com>
2026-08-11 17:30:39 -0300

    chore: remove SQL transitorio

--- supabase/auditoria/limpar-orfa-teste.sql
+++
@@ -1,2 +0,0 @@
-with alvo as (select id, email from auth.users where lower(email)=lower('teste.acesso@maradel.com.br'))
-delete from auth.users u using alvo a where u.id = a.id returning a.email as removida;
